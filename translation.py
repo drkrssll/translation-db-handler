8911f158e8a8ed8c9329e3db1969aa4bd34b7f8b
drkrssll <russellderek01@gmail.com>
2024-12-20 23:40:58 -0500

    Added a new table for phrases

--- translation.py
+++ translation.py
@@ -8,23 +8,42 @@ cursor.execute('''
         id INTEGER PRIMARY KEY,
         source_word TEXT NOT NULL,
         target_word TEXT NOT NULL
-    )
+    );
 ''')
+
+cursor.execute('''
+    CREATE TABLE IF NOT EXISTS phrases (
+        id INTEGER PRIMARY KEY,
+        source_phrase TEXT NOT NULL,
+        target_phrase TEXT NOT NULL
+    );
+''')
+
+def add_phrase(source, target):
+    cursor.execute('INSERT INTO phrases (source_phrase, target_phrase) VALUES (?, ?)', (source, target))
+    conn.commit()
 
 def add_pair(source, target):
     cursor.execute('INSERT INTO word_pairs (source_word, target_word) VALUES (?, ?)', (source, target))
     conn.commit()
 
+def get_phrases():
+    cursor.execute('SELECT * FROM phrases')
+    return cursor.fetchall()
 
 def get_pairs():
     cursor.execute('SELECT * FROM word_pairs')
     return cursor.fetchall()
 
-# take input from user and add pairs to database
 while True:
+    table = input('Enter table name: ')
     source = input('Enter source word: ')
     target = input('Enter target word: ')
 
-    add_pair(source, target)
+    if table == 'phrases':
+        add_phrase(source, target)
+        print(get_phrases())
+    elif table == 'word_pairs':
+        add_pair(source, target)
+        print(get_pairs())
 
-    print(get_pairs())
